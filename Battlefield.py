4cb4854311561c8ea9d009a9bd1892db8bad6d36
TGWolf <60626007+sfodd1@users.noreply.github.com>
2021-09-08 14:43:53 -0400

    class instantiation structure created and display_welcome function working as intended

--- Battlefield.py
+++ Battlefield.py
@@ -3,12 +3,14 @@ import Herd,Fleet
 class Battlefield:
     def __init__(self):
         print("Battlefield sequence initiating..")
+        self.display_welcome()
         self.herd = Herd()
         self.fleet = Fleet()
 
 
     def display_welcome(self):
-        pass
+        print("________________________________________________________________________________\n\t"
+        + "Welcome to the 3D combat simulator where your fighting dreams become reality!")
 
     def battle(self):
         pass
